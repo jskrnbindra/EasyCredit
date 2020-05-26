b416b434d806c4f9d9304e34e8b063019d69139c
Bindra <jskrnbindra@gmail.com>
2020-05-26 22:54:36 +0530

    Send flow hollow ready

--- mobile/app/src/main/java/com/easycredit/ui/send/SendMoneyActivity.java
+++ mobile/app/src/main/java/com/easycredit/ui/send/SendMoneyActivity.java
@@ -1,9 +1,9 @@
 package com.easycredit.ui.send;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,7 +18,6 @@ import android.widget.Toast;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
 import com.easycredit.R;
 import com.easycredit.data.Http;
 import com.easycredit.data.model.EasyCreditUser;
@@ -37,10 +36,21 @@ public class SendMoneyActivity extends AppCompatActivity {
 
     Button verifyButton;
     Button sendButton;
+    Button confirmButton;
+    Button cancelInVerified;
+    Button cancelInSend;
     EditText beneficiaryPhoneEditText;
     EditText amountEditText;
     ProgressBar topProgressBar;
     TextView header;
+    TextView verificationMessage;
+    TextView beneficiaryName;
+    TextView beneficiaryPhone;
+    TextView beneficiaryNameInSend;
+    TextView beneficiaryPhoneInSend;
+    ConstraintLayout verifyBox;
+    ConstraintLayout verifiedBox;
+    ConstraintLayout sendBox;
 
 
     // Listeners
@@ -62,7 +72,7 @@ public class SendMoneyActivity extends AppCompatActivity {
     View.OnClickListener verifyOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Log.i(TAG, "onClick: verify phone");
+            Log.d(TAG, "onClick: verify phone");
             topProgressBar.setVisibility(View.VISIBLE);
             beneficiaryPhoneEditText.setActivated(false);
             verifyButton.setActivated(false);
@@ -73,12 +83,45 @@ public class SendMoneyActivity extends AppCompatActivity {
                     getString(R.string.users_func_key),
                     beneficiaryPhone);
             JsonObjectRequest request = new JsonObjectRequest(url, null,
-                    userFetched(), requestFailed(getApplicationContext(),
+                    beneficiaryFetched(), requestFailed(getApplicationContext(),
                     "getUserByPhone"));
             http.add(request);
         }
     };
 
+    View.OnClickListener cancelInVerifiedOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            verifyBox.setVisibility(View.VISIBLE);
+            verifiedBox.setVisibility(View.GONE);
+            verificationMessage.setVisibility(View.INVISIBLE);
+        }
+    };
+
+    View.OnClickListener confirmOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            verifiedBox.setVisibility(View.GONE);
+            sendBox.setVisibility(View.VISIBLE);
+        }
+    };
+
+    View.OnClickListener sendOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), R.string.send_dough, Toast.LENGTH_LONG).show();
+        }
+    };
+
+    View.OnClickListener cancelInSendOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            verifyBox.setVisibility(View.VISIBLE);
+            verifiedBox.setVisibility(View.GONE);
+            sendBox.setVisibility(View.GONE);
+            verificationMessage.setVisibility(View.INVISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,30 +131,47 @@ public class SendMoneyActivity extends AppCompatActivity {
         http = Http.getInstance(this);
         userId = getIntent().getStringExtra(getString(R.string.user_id_extra));
 
-
         verifyButton = findViewById(R.id.verifyButton);
         sendButton = findViewById(R.id.sendButton);
+        cancelInVerified = findViewById(R.id.cancelButton);
+        cancelInSend = findViewById(R.id.cancelButtonInSendBox);
         beneficiaryPhoneEditText = findViewById(R.id.beneficiaryPhone);
         amountEditText = findViewById(R.id.amount);
         topProgressBar = findViewById(R.id.sendProgressBar);
         header = findViewById(R.id.header);
+        verifyBox = findViewById(R.id.verifyBox);
+        verifiedBox = findViewById(R.id.verifiedBox);
+        sendBox = findViewById(R.id.sendBox);
+        verificationMessage = findViewById(R.id.verificationMessage);
+        beneficiaryName = findViewById(R.id.beneficiaryName);
+        beneficiaryPhone = findViewById(R.id.beneficiaryPhoneTV);
+        confirmButton = findViewById(R.id.confirmButton);
+        beneficiaryNameInSend = findViewById(R.id.beneficiaryNameInSendBox);
+        beneficiaryPhoneInSend = findViewById(R.id.beneficiaryPhoneTVInSendBox);
 
         beneficiaryPhoneEditText.addTextChangedListener(beneficiaryPhoneTextWatcher);
         verifyButton.setOnClickListener(verifyOnClickListener);
+        cancelInVerified.setOnClickListener(cancelInVerifiedOnClickListener);
+        confirmButton.setOnClickListener(confirmOnClickListener);
+        cancelInSend.setOnClickListener(cancelInSendOnClickListener);
+        sendButton.setOnClickListener(sendOnClickListener);
     }
 
-    private Response.Listener<JSONObject> userFetched()
+    private Response.Listener<JSONObject> beneficiaryFetched()
     {
         return new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 EasyCreditUser beneficiary = GSON.fromJson(response.toString(), EasyCreditUser.class);
                 Log.d(TAG, "beneficiary fetched -> " + beneficiary);
-                header.setText(beneficiary.getName());
-                verifyButton.setVisibility(View.GONE);
-                beneficiaryPhoneEditText.setVisibility(View.GONE);
-                sendButton.setVisibility(View.VISIBLE);
-                amountEditText.setVisibility(View.VISIBLE);
+
+                beneficiaryName.setText(beneficiary.getName());
+                beneficiaryPhone.setText(beneficiary.getPhone());
+                beneficiaryNameInSend.setText(beneficiary.getName());
+                beneficiaryPhoneInSend.setText(beneficiary.getPhone());
+
+                verifyBox.setVisibility(View.GONE);
+                verifiedBox.setVisibility(View.VISIBLE);
                 topProgressBar.setVisibility(View.INVISIBLE);
             }
         };
@@ -123,9 +183,9 @@ public class SendMoneyActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d(TAG, requestName + " failed: " + error.getMessage());
-                header.setText("Not found!");
                 verifyButton.setActivated(true);
                 beneficiaryPhoneEditText.setActivated(true);
+                verificationMessage.setVisibility(View.VISIBLE);
                 topProgressBar.setVisibility(View.INVISIBLE);
             }
         };
